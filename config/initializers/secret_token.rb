a095c57c9ee5bc247e6bb113c691e05fc7c9abb3
Yusuf Saber <yusuf.saber@hotmail.com>
2011-08-17 22:49:10 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaveApp::Application.config.secret_token = 'af6528df280ddd8035f1faf242820c528489035c220c33ee3e1447dd7dcf4a430d255fd63468307ab76357517d80991697cbcee25529b8c15c2d46ff6e7b94e8'
