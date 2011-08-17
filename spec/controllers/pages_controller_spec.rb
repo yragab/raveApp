263dc646a68cff1c02d771d2e0013085a3ca0574
Yusuf Saber <yusuf.saber@hotmail.com>
2011-08-18 01:04:37 +0200

    Getting rid of title test

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,40 +7,31 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
-  end
-  
-  it "should have the right title" do
-      get 'home'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+    it "should have the right title" do
+      get "home"
+      response.should have_selector("title", :content => "Home")
     end
   end
-
+  
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
-  end
-  
-  it "should have the right title" do
-      get 'about'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | About")
+    it "should have the right title" do
+      get "about"
+      response.should have_selector("title", :content => "About")
     end
   end
-  
+ 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
-  end
-
-  it "should have the right title" do
-      get 'contact'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Contact")
+    it "should have the right title" do
+      get "contact"
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
   
