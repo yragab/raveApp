ad4b8393a38206fd0e0d07d972e7eb2d36d82f62
Yusuf Saber <yusuf.saber@hotmail.com>
2011-08-21 03:14:16 +0200

    Hoping

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
     @title = "Sign up"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Rave App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      @user.password = ""
+      @user.password_confirmation = ""
+      render 'new'
+    end
   end
   
   def show
